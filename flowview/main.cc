8bf3ec1c7dfd2df9abe32ff1dfea8179b1ac5c4e
ueshiba <ueshiba@3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38>
2009-08-12 00:21:22 +0000

    入力ムービーのview数だけのキャンバスを生成するようにした．
    git-svn-id: svn+ssh://cv077/home/ueshiba/svnroot/trunk/tools@630 3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38

--- flowview/main.cc
+++ flowview/main.cc
@@ -1,5 +1,5 @@
 /*
- *  $Id: main.cc,v 1.5 2009-08-11 00:45:53 ueshiba Exp $
+ *  $Id: main.cc,v 1.6 2009-08-12 00:21:22 ueshiba Exp $
  */
 #include <stdlib.h>
 #include "TU/v/App.h"
@@ -19,21 +19,28 @@ template <class T>
 class MyCanvasPane : public CanvasPane
 {
   public:
-    MyCanvasPane(Window& parentWin, const Image<T>& image)
+    MyCanvasPane(Window& parentWin, Image<T>& image)
 	:CanvasPane(parentWin, image.width(), image.height()),
 	 _dc(*this), _image(image)					{}
-    
+
+    Image<T>&		image()						;
     virtual void	repaintUnderlay()				;
     
   private:
 #ifdef USE_XVDC
-    XvDC		_dc;
+    XvDC	_dc;
 #else
-    ShmDC		_dc;
+    ShmDC	_dc;
 #endif
-    const Image<T>&	_image;
+    Image<T>&	_image;
 };
 
+template <class T> Image<T>&
+MyCanvasPane<T>::image()
+{
+    return _image;
+}
+        
 template <class T> void
 MyCanvasPane<T>::repaintUnderlay()
 {
@@ -49,14 +56,12 @@ class MyCmdWindow : public CmdWindow
   public:
     MyCmdWindow(App& parentApp, const char* name,
 		Array<Image<T> >& images)				;
-
-    void		tick()						;
+    ~MyCmdWindow()							;
+    
+    void	tick()							;
 
   private:
-    Array<Image<T> >&		_images;
-    MyCanvasPane<T>		_canvas0;
-    MyCanvasPane<T>		_canvas1;
-    MyCanvasPane<T>		_canvas2;
+    Array<MyCanvasPane<T>*>	_canvases;
     Timer			_timer;
 };
 
@@ -64,17 +69,23 @@ template <class T>
 MyCmdWindow<T>::MyCmdWindow(App& parentApp, const char* name,
 			    Array<Image<T> >& images)
     :CmdWindow(parentApp, name, 0, Colormap::RGBColor, 16, 0, 0),
-     _images(images),
-     _canvas0(*this, _images[0]),
-     _canvas1(*this, _images[1]),
-     _canvas2(*this, _images[2]),
+     _canvases(images.dim()),
      _timer(*this, 10)
 {
-    _canvas0.place(0, 0, 1, 1);
-    _canvas1.place(1, 0, 1, 1);
-    _canvas2.place(2, 0, 1, 1);
+    for (u_int i = 0; i < _canvases.dim(); ++i)
+    {
+	_canvases[i] = new MyCanvasPane<T>(*this, images[i]);
+	_canvases[i]->place(i % 2, i / 2, 1, 1);
+    }
     
     show();
+}
+
+template <class T>
+MyCmdWindow<T>::~MyCmdWindow()
+{
+    for (u_int i = 0; i < _canvases.dim(); ++i)
+	delete _canvases[i];
 }
  
 template <class T> void
@@ -82,11 +93,11 @@ MyCmdWindow<T>::tick()
 {
     using namespace	std;
 
-    for (int i = 0; i < _images.dim(); ++i)
-	_images[i].restoreData(cin);
-    _canvas0.repaintUnderlay();
-    _canvas1.repaintUnderlay();
-    _canvas2.repaintUnderlay();
+    for (int i = 0; i < _canvases.dim(); ++i)
+    {
+	_canvases[i]->image().restoreData(cin);
+	_canvases[i]->repaintUnderlay();
+    }
 }
 
 }
