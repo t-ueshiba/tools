64211d31f3222bbe29773659ba8e2387f5c380a2
ueshiba <ueshiba@3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38>
2009-08-12 04:32:40 +0000

    1列に表示するview数を指定できるようにした．
    git-svn-id: svn+ssh://cv077/home/ueshiba/svnroot/trunk/tools@631 3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38

--- flowview/main.cc
+++ flowview/main.cc
@@ -1,5 +1,5 @@
 /*
- *  $Id: main.cc,v 1.6 2009-08-12 00:21:22 ueshiba Exp $
+ *  $Id: main.cc,v 1.7 2009-08-12 04:32:40 ueshiba Exp $
  */
 #include <stdlib.h>
 #include "TU/v/App.h"
@@ -55,7 +55,7 @@ class MyCmdWindow : public CmdWindow
 {
   public:
     MyCmdWindow(App& parentApp, const char* name,
-		Array<Image<T> >& images)				;
+		Array<Image<T> >& images, u_int ncol)			;
     ~MyCmdWindow()							;
     
     void	tick()							;
@@ -67,7 +67,7 @@ class MyCmdWindow : public CmdWindow
 
 template <class T>
 MyCmdWindow<T>::MyCmdWindow(App& parentApp, const char* name,
-			    Array<Image<T> >& images)
+			    Array<Image<T> >& images, u_int ncol)
     :CmdWindow(parentApp, name, 0, Colormap::RGBColor, 16, 0, 0),
      _canvases(images.dim()),
      _timer(*this, 10)
@@ -75,7 +75,7 @@ MyCmdWindow<T>::MyCmdWindow(App& parentApp, const char* name,
     for (u_int i = 0; i < _canvases.dim(); ++i)
     {
 	_canvases[i] = new MyCanvasPane<T>(*this, images[i]);
-	_canvases[i]->place(i % 2, i / 2, 1, 1);
+	_canvases[i]->place(i % ncol, i / ncol, 1, 1);
     }
     
     show();
@@ -106,7 +106,7 @@ MyCmdWindow<T>::tick()
 *  static functions							*
 ************************************************************************/
 template <class T> static void
-doJob(v::App& vapp, const Array<GenericImage>& headers)
+doJob(v::App& vapp, const Array<GenericImage>& headers, u_int ncol)
 {
   // ヘッダ情報に基づいて画像サイズを設定．
     Array<Image<T> >	images(std::max(headers.dim(), 3u));
@@ -114,7 +114,7 @@ doJob(v::App& vapp, const Array<GenericImage>& headers)
 	images[i].resize(headers[i].height(), headers[i].width());
 	
   // GUIオブジェクトを作り，イベントループを起動．
-    v::MyCmdWindow<T>	myWin(vapp, "Real-time image viewer", images);
+    v::MyCmdWindow<T>	myWin(vapp, "Real-time image viewer", images, ncol);
     vapp.run();
 }
     
@@ -128,8 +128,17 @@ main(int argc, char* argv[])
     using namespace	std;
     using namespace	TU;
     
-    v::App	vapp(argc, argv);
-
+    v::App		vapp(argc, argv);
+    u_int		ncol = 2;
+    extern char*	optarg;
+    for (int c; (c = getopt(argc, argv, "c:")) !=EOF; )
+	switch (c)
+	{
+	  case 'c':
+	    ncol = atoi(optarg);
+	    break;
+	}
+    
   // 1フレームあたりの画像数を取得．
     char	c;
     if (!cin.get(c) || (c != 'M'))
@@ -153,19 +162,19 @@ main(int argc, char* argv[])
     switch (headers[0].type())
     {
       case ImageBase::U_CHAR:
-	doJob<u_char>(vapp, headers);
+	doJob<u_char>(vapp, headers, ncol);
 	break;
       case ImageBase::RGB_24:
-	doJob<RGBA>(vapp, headers);
+	doJob<RGBA>(vapp, headers, ncol);
 	break;
       case ImageBase::YUV_444:
-	doJob<YUV444>(vapp, headers);
+	doJob<YUV444>(vapp, headers, ncol);
 	break;
       case ImageBase::YUV_422:
-	doJob<YUV422>(vapp, headers);
+	doJob<YUV422>(vapp, headers, ncol);
 	break;
       case ImageBase::YUV_411:
-	doJob<YUV411>(vapp, headers);
+	doJob<YUV411>(vapp, headers, ncol);
 	break;
     }
     
