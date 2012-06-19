741e49dd67a82e7ab0a6209f89964f368e1b0acb
ueshiba <ueshiba@3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38>
2012-06-19 06:18:59 +0000

    V4L2�����������鴻�������羌��flowV4L2camera�������若��с�
    git-svn-id: svn+ssh://cv077/home/ueshiba/svnroot/trunk/tools@1091 3ef1b8b6-fb57-41bf-88d9-9a5bd220bf38

---
+++ flowV4L2camera/main.cc
@@ -0,0 +1,162 @@
+/*
+ *  $Id: main.cc,v 1.1 2012-06-19 06:18:59 ueshiba Exp $
+ */
+#include <signal.h>
+#include <sys/time.h>
+#include <cstdlib>
+#include <algorithm>
+#include <iterator>
+#include <stdexcept>
+#include <boost/foreach.hpp>
+#include "TU/V4L2++.h"
+
+namespace TU
+{
+/************************************************************************
+*  static functions							*
+************************************************************************/
+static void
+usage(const char* s)
+{
+    using namespace	std;
+
+    cerr << "\nPut image stream from UVC cameras to stdout.\n"
+	 << endl;
+    cerr << " Usage: " << s << " [options]\n"
+	 << endl;
+}
+
+static bool	active = true;
+
+static void
+handler(int sig)
+{
+    using namespace	std;
+    
+    cerr << "Signal [" << sig << "] caught!" << endl;
+    active = false;
+}
+
+template <class T> static void
+doJob(V4L2Camera& camera)
+{
+    using namespace	std;
+
+  // Set signal handler.
+    signal(SIGINT,  handler);
+    signal(SIGPIPE, handler);
+
+    Image<T>	image(camera.width(), camera.height());
+
+  // 1フレームあたりの画像数とそのフォーマットを出力．
+    cout << 'M' << 1 << endl;
+    image.saveHeader(cout);
+
+    camera.continuousShot();
+    
+    int		nframes = 0;
+    timeval	start;
+    while (active)
+    {
+	if (nframes == 10)
+	{
+	    timeval      end;
+	    gettimeofday(&end, NULL);
+	    double	interval = (end.tv_sec  - start.tv_sec) +
+				   (end.tv_usec - start.tv_usec) / 1.0e6;
+	    cerr << nframes / interval << " frames/sec" << endl;
+	    nframes = 0;
+	}
+	if (nframes++ == 0)
+	    gettimeofday(&start, NULL);
+
+	camera.snap();				// 撮影
+      //camera >> image;			// 画像領域への転送
+	camera.captureDirectly(image);		// 画像領域への転送
+	if (!image.saveData(cout))		// stdoutへの出力
+	    active = false;
+    }
+}
+
+}
+/************************************************************************
+*  global functions							*
+************************************************************************/
+int
+main(int argc, char* argv[])
+{
+    using namespace	std;
+    using namespace	TU;
+    
+    const char*		deviceName = "/dev/video0";
+    extern char*	optarg;
+    for (int c; (c = getopt(argc, argv, "d:h")) != -1; )
+	switch (c)
+	{
+	  case 'd':
+	    deviceName = optarg;
+	    break;
+	  case 'h':
+	    usage(argv[0]);
+	    return 1;
+	}
+    
+    try
+    {
+      // UVCカメラのオープン．
+	V4L2Camera	camera(deviceName);
+#if 1
+	BOOST_FOREACH (V4L2Camera::PixelFormat pixelFormat,
+		       camera.availablePixelFormats())
+	    camera.put(cerr, pixelFormat);
+	BOOST_FOREACH (V4L2Camera::Feature feature,
+		       camera.availableFeatures())
+	    camera.put(cerr, feature);
+
+	cerr << camera.width() << 'x' << camera.height()
+	     << ':' << hex << camera.pixelFormat() << dec << endl;
+	cerr << "Brightness = "
+	     << camera.getValue(V4L2Camera::BRIGHTNESS) << endl;
+	camera.setValue(V4L2Camera::BRIGHTNESS, 64);
+	cerr << "Brightness(after setting) = "
+	     << camera.getValue(V4L2Camera::BRIGHTNESS) << endl;
+
+	camera.setFormat(V4L2Camera::UYVY, 640, 480, 1, 30);
+	cerr << camera.width() << 'x' << camera.height()
+	     << ':' << hex << camera.pixelFormat() << dec << endl;
+#endif	
+      // 画像のキャプチャと出力．
+	switch (camera.pixelFormat())
+	{
+	  case V4L2Camera::GREY:
+	    doJob<u_char>(camera);
+	    break;
+	  case V4L2Camera::UYVY:
+	    doJob<YUV422>(camera);
+	    break;
+	  case V4L2Camera::RGB24:
+	    doJob<RGB>(camera);
+	    break;
+	  case V4L2Camera::BGR24:
+	    doJob<BGR>(camera);
+	    break;
+	  case V4L2Camera::RGB32:
+	    doJob<RGBA>(camera);
+	    break;
+	  case V4L2Camera::BGR32:
+	    doJob<ABGR>(camera);
+	    break;
+	  default:
+	    throw runtime_error("Unsupported pixel format!!");
+	    break;
+	}
+
+	cerr << camera;
+    }
+    catch (exception& err)
+    {
+	cerr << err.what() << endl;
+    }
+    
+    return 0;
+}
